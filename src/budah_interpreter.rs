41de9a4c5283292bc563d311561c1fa0ee728397
tomdrc1 <tomdrc4@gmail.com>
2022-01-16 12:03:28 +0200

    Added opcodes for Budah lang

--- src/budah_interpreter.rs
+++ src/budah_interpreter.rs
@@ -1,4 +1,5 @@
 pub mod string_handler;
+mod io_handler;
 
 const MEMORY_SIZE: usize = 0x10000;
 
@@ -17,9 +18,57 @@ impl BudahInterpreter
     }
 
     /// Will interpert the current line
-    pub fn interpert(current_line: String)
+    /// 
+    /// # Arguments
+    /// * `current_line` - the current line that we want to interpret
+    pub fn interpert(&mut self, current_line: String)
     {
         let split_words = string_handler::split_words(current_line);
         
+        for word in split_words
+        {
+            match word.as_str() {
+                "Budah" => self.decrement_index(),
+                "budaH" => self.increment_index(),
+                "BUDAH" => self.read(),
+                "bUdah" => self.decrement(),
+                "budAh" => self.increment(),
+                "budah" => self.print(),
+                _ => ()
+            }
+        }
+    }
+
+    fn decrement_index(&mut self)
+    {
+        self.index -= 1;
+    }
+
+    fn increment_index(&mut self)
+    {
+        self.index += 1;
+    }
+
+    fn decrement(&mut self)
+    {
+        self.memory[self.index as usize] -= 1;
+    }
+
+    fn increment(&mut self)
+    {
+        self.memory[self.index as usize] += 1;
+    }
+
+    fn read(&mut self)
+    {
+        print!("Please enter a value: ");
+        let value = io_handler::read_char();
+
+        self.memory[self.index as usize] = value;
+    }
+
+    fn print(&self)
+    {
+        print!("{}", self.memory[self.index as usize]);
     }
 }
